65e235a779309fa53ea1b54cdaf52530911e6786
Victor Soares Ribeiro <vribeiro@serramar.local>
2021-12-06 18:17:29 -0300

    login

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const path = require('path');
-
+const passport = require('passport');
+const session = require('express-session');
 const app = express();
 const server = require('http').createServer(app); 			//DECLARA PROTOCOLO HTTP
-const io = require('socket.io')(server);		  			//DECLARA PROTOCOLO WSS - - > WEB SOCKET SERVER
+const io = require('socket.io')(server);					//DECLARA PROTOCOLO WSS - - > WEB SOCKET SERVER
+
+
+require('./auth')(passport);
+app.use(session({
+	secret: '123',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {maxAge: 30 * 60 * 1000}
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+		  			
 
 
 app.use(express.static(path.join(__dirname, 'public')));  	//DEFINE PASTA ONDE VAI FICAR OS ARQUIVOS PUBLICO  - - > HTML, EJS
@@ -25,9 +39,27 @@ app.get('/', async (req, res) => {
 	res.render('index', { docs });
 });
 
+app.get('/login', (req, res) => {
+	res.render('login');
+})
+
 app.get('/cadastro', (req, res) => {
 	res.render('cadastro', { title: 'Cadastro de usuário: chat Serramar' })
-})
+});
+
+app.get('/login', (req, res, next) => {
+	if (req.query.fail)
+		res.render('login', { message: 'Usuário e/ou senha incorretos!' });
+	else
+		res.render('login', { message: null })
+});
+
+app.post('/login',
+	passport.authenticate('local', {
+		sucessRedirect: 'index',
+		failureRedirect: '/login?fail=true'
+	})
+)
 
 
 app.post('/cadastro', async (req, res) => {
